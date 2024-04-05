7edb7f23423a310db5cd969b88b09b8db6d14f32
Thatcher Smith <thatcher.smith@slalom.com>
2024-04-05 16:49:21 -0600

    Add basic k8s deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,6 +32,98 @@ provider "aws" {
   }
 }
 
-resource "aws_s3_bucket" "resource" {
-  bucket = "demo-harness-terraform-bucket"
+data "aws_vpc" "resource" {
+  id = "vpc-01f5dce028f371283"
+}
+
+data "aws_subnets" "resource" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.resource]
+  }
+
+  filter {
+    name   = "cidr-block"
+    values = [
+      "10.0.101.0/24",
+      "10.0.102.0/24",
+      "10.0.103.0/24"
+    ]
+  }
+}
+
+
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = "demo-app-cluster"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+    eks-pod-identity-agent = {
+      most_recent = true
+    }
+  }
+
+  vpc_id     = data.aws_vpc.resource.id
+  subnet_ids = data.aws_subnets.resource.ids
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.large"]
+  }
+
+  eks_managed_node_groups = {
+    demo = {
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    # One access entry with a policy associated
+    demo = {
+      kubernetes_groups = [
+        "cluster-admin",
+      ]
+      principal_arn     = "arn:aws:iam::050012553741:user/thatcher"
+
+      policy_associations = {
+        namespace = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            type       = "namespace"
+            namespaces = [
+              "default",
+              "kube-node-lease",
+              "kube-public",
+              "kube-system",
+              "harness-delegate-ng",
+            ]
+          }
+        }
+      }
+    }
+  }
 }
